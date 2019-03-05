18bc6ac20602c595ce1d21941e05ca39bef8ab02
Jan Neduchal <honza.neduchal@gmail.com>
2019-03-05 10:39:28 +0100

    Fixed exceptions, added some more output

--- buster.py
+++ buster.py
@@ -9,28 +9,6 @@ import socket
 from sty import fg, rs
 
 
-parser = argparse.ArgumentParser(description='Simple tool for domain busting')
-
-parser.add_argument('-d', '--domain', help='Target domain / file' ,required=True)
-parser.add_argument('-t', '--timeout', default=2, type=int, help='Timeout treshold, default is "2" sec')
-parser.add_argument('-o', '--output', default='result.txt', help='Name for the file output. Default is "result.txt"')
-
-args = parser.parse_args()
-if not re.findall(r"^https?://", args.domain):
-    domain = "http://" + args.domain
-else:
-    domain = args.domain
-
-try:
-    r = requests.get(domain, timeout=args.timeout)
-    if r.status_code == 404:
-        sys.exit("Domain returns HTTP 404")
-except requests.exceptions.Timeout:
-    sys.exit("Domain times out, maybe try to increase -t")
-except:
-    print(domain)
-    sys.exit("A major fuckup happened, please submit an issue on github")
-
 def isIP(domain):
     ips = re.findall(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])", domain)
     if len(ips) == 0:
@@ -39,7 +17,8 @@ def isIP(domain):
         return 1
 
 
-def scanForDomains(path):
+def scanForDomains(args, path):
+    found = 0
     try:
         r = requests.get(path, timeout=args.timeout)
     except requests.exceptions.Timeout:
@@ -50,13 +29,42 @@ def scanForDomains(path):
             continue
         if isIP(i):
             print(fg(72,251,0) + "FOUND IP: " + fg.rs + i)
+            found += 1
             continue
         try:
             socket.gethostbyname(i)
         except socket.gaierror:
             print(fg(72,251,0) + "COULD NOT RESOLVE DNS: " + fg.rs + i)
+            found += 1
         except:
             pass
     #print(domains)
+    if found == 0:
+        print(fg(72,251,0) + "Didn't find any usefull stuff :(" + fg.rs)
 if __name__ == "__main__":
-    scanForDomains(domain)
+
+    parser = argparse.ArgumentParser(description='Simple tool for domain busting')
+
+    parser.add_argument('-d', '--domain', help='Target domain / file' ,required=True)
+    parser.add_argument('-t', '--timeout', default=2, type=int, help='Timeout treshold, default is "2" sec')
+    parser.add_argument('-o', '--output', default='result.txt', help='Name for the file output. Default is "result.txt"')
+
+    args = parser.parse_args()
+    if not re.findall(r"^https?://", args.domain):
+        domain = "http://" + args.domain
+    else:
+        domain = args.domain
+
+    try:
+        r = requests.get(domain, timeout=args.timeout)
+        if r.status_code == 404:
+            sys.exit("Domain returns HTTP 404")
+    except requests.exceptions.Timeout:
+        sys.exit("Domain times out, maybe try to increase -t")
+    except requests.exceptions.ConnectionError:
+        sys.exit("Server failed to respond, this is not a timeout")
+    except:
+        print(domain)
+        sys.exit("A major fuckup happened, please submit an issue on github")
+
+    scanForDomains(args, domain)
