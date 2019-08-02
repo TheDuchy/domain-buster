ae4f5f99c99d3215b7c1457524ef555c8b12cb53
Jan Neduchal <honza.neduchal@gmail.com>
2019-08-03 00:19:30 +0200

    Update to follow pep8

--- buster.py
+++ buster.py
@@ -1,8 +1,8 @@
-#Written by Jan "Duchy" Neduchal 2019
-#This code is under the IDFC Licence.
-#The IDFC License means do whatever you want just give credit
+# Written by Jan "Duchy" Neduchal 2019
+# This code is under the IDFC Licence.
+# The IDFC License means do whatever you want just give credit
 import requests
-import argparse
+from argparse import ArgumentParser
 import sys
 import re
 import socket
@@ -10,7 +10,8 @@ from sty import fg, rs
 
 
 def isIP(domain):
-    ips = re.findall(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])", domain)
+    ips = re.findall(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}\
+                    ([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])", domain)
     if len(ips) == 0:
         return 0
     else:
@@ -28,33 +29,35 @@ def scanForDomains(args, path):
         if len(i) == 0:
             continue
         if isIP(i):
-            print(fg(72,251,0) + "FOUND IP: " + fg.rs + i)
+            print(fg(72, 251, 0) + "FOUND IP: " + fg.rs + i)
             found += 1
             continue
         try:
             socket.gethostbyname(i)
         except socket.gaierror:
-            print(fg(72,251,0) + "COULD NOT RESOLVE DNS: " + fg.rs + i)
+            print(fg(72, 251, 0) + "COULD NOT RESOLVE DNS: " + fg.rs + i)
             found += 1
         except:
             pass
-    #print(domains)
     if found == 0:
-        print(fg(72,251,0) + "Didn't find any usefull stuff :(" + fg.rs)
+        print(fg(72, 251, 0) + "Didn't find any usefull stuff :(" + fg.rs)
+
+
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Simple tool for domain busting')
-
-    parser.add_argument('-d', '--domain', help='Target domain / file' ,required=True)
-    parser.add_argument('-t', '--timeout', default=2, type=int, help='Timeout treshold, default is "2" sec')
-    parser.add_argument('-o', '--output', default='result.txt', help='Name for the file output. Default is "result.txt"')
-
+    parser = ArgumentParser(description='A tool for recursive domain busting')
+    parser.add_argument('-d', '--domain', help='Target domain / file',
+                        required=True)
+    parser.add_argument('-t', '--timeout', default=2, type=int,
+                        help='Timeout treshold (sec). ["2"]')
+    parser.add_argument('-o', '--output', default='result.txt',
+                        help='Name for the file output. ["result.txt"]')
     args = parser.parse_args()
+
     if not re.findall(r"^https?://", args.domain):
         domain = "http://" + args.domain
     else:
         domain = args.domain
-
     try:
         r = requests.get(domain, timeout=args.timeout)
         if r.status_code == 404:
@@ -63,8 +66,8 @@ if __name__ == "__main__":
         sys.exit("Domain times out, maybe try to increase -t")
     except requests.exceptions.ConnectionError:
         sys.exit("Server failed to respond, this is not a timeout")
-    except:
-        print(domain)
+    except Exception as ex:
+        print("Exception: %s" % ex)
         sys.exit("A major fuckup happened, please submit an issue on github")
 
     scanForDomains(args, domain)
